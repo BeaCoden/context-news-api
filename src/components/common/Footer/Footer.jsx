64b3208c882995940f120a138b6670f27d18a44e
Bea Pitzschke <bea.pitzschke.coden@gmail.com>
2024-08-23 22:57:26 +0200

    chore: Add footer component to the project

--- src/components/common/Footer/Footer.jsx
+++ src/components/common/Footer/Footer.jsx
@@ -4,6 +4,7 @@ import styles from "./Footer.module.css";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
+      <h1>Footer</h1>
       <p>© 2021. All rights reserved</p>
     </footer>
   );
