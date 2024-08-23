48d196e5f4e2b6d8989038aac9580361c3b714ef
Bea Pitzschke <bea.pitzschke.coden@gmail.com>
2024-08-23 22:58:05 +0200

    refactor: Update routing and add components for improved navigation

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "../components/common/header/Header";
+import Home from "../pages/home/Home";
+import About from "../pages/about/About";
+import Categories from "../pages/categories/Categories";
+import Contact from "../pages/contact/Contact";
+import Search from "../pages/search/Search";
 
 const AppRouter = () => {
-  return <div>AppRouter</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route
+            path="/"
+            element={<Home />}
+          />
+          <Route
+            path="/about"
+            element={<About />}
+          />
+          <Route
+            path="/categories"
+            element={<Categories />}
+          />
+          <Route
+            path="/contact"
+            element={<Contact />}
+          />
+          <Route
+            path="/search"
+            element={<Search />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default AppRouter;
