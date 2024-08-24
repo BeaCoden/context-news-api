ddf5e8b986de731ce2e87de94365db63bf5375dc
Bea Pitzschke <bea.pitzschke.coden@gmail.com>
2024-08-24 08:33:05 +0200

    refactor: Add search functionality & styling to Header component

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,11 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Header.module.css";
+import { useNavigate } from "react-router-dom";
+// import Navbar from "../navbar/Navbar";
 
 const Header = () => {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate("/search", { state: search });
+    setSearch("");
+  };
+
   return (
-    <header className={styles.header}>
-      <h1>Header</h1>
-    </header>
+    <div className={styles.container}>
+      <h1>
+        <em>news</em>
+      </h1>
+      <div className={styles.top}>
+        <form
+          onSubmit={handleSubmit}
+          className={styles["search-bar"]}>
+          <div className={styles["input-wrapper"]}>
+            <input
+              type="search"
+              name="search"
+              pattern=".*\S.*"
+              required
+              className={`${styles.search} ${styles["search-input"]}`}
+              value={search}
+              placeholder="Search your news..."
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            <button
+              type="submit"
+              className={styles["search-btn"]}>
+              🔍
+            </button>
+          </div>
+        </form>
+      </div>
+      {/* <Navbar /> */}
+    </div>
   );
 };
 
