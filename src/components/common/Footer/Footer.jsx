152bac876ec467caf15018b6d6e75e291463bc75
Bea Pitzschke <bea.pitzschke.coden@gmail.com>
2024-08-25 00:53:18 +0200

    refactor: Update Footer component with additional links and styling

--- src/components/common/Footer/Footer.jsx
+++ src/components/common/Footer/Footer.jsx
@@ -1,11 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styles from "./Footer.module.css";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <h1>Footer</h1>
+      <h1>ℹ️</h1>
       <p>© 2021. All rights reserved</p>
+      <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/username"
+            target="_blank"
+            rel="noreferrer">
+            LinkedIn
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.github.com/username"
+            target="_blank"
+            rel="noreferrer">
+            GitHub
+          </a>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
     </footer>
   );
 };
