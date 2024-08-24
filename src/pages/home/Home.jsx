8426363f997175292acf0a92253b75862b59bfeb
Bea Pitzschke <bea.pitzschke.coden@gmail.com>
2024-08-25 00:52:49 +0200

    refactor: Add Footer component to Home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from "./Home.module.css";
+import Footer from "../../components/common/footer/Footer";
 
 const Home = () => {
   return (
     <div className={styles.home}>
       <h2>Home</h2>
+      <Footer />
     </div>
   );
 };
