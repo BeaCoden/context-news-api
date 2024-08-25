f345155c06dfd9e0c9c15c2650250a2dd24e6ac2
Bea Pitzschke <bea.pitzschke.coden@gmail.com>
2024-08-25 23:29:35 +0200

    refactor: Update Navbar component to include Newsletter link

--- src/components/common/navbar/Navbar.jsx
+++ src/components/common/navbar/Navbar.jsx
@@ -18,23 +18,7 @@ const Navbar = () => {
             <li onClick={() => navigate("/categories", { state: { category: "science" } })}>Science</li>
           </ul>
         </li>
-        <li>
-          <p>Contact</p>
-          <ul className={styles.bottomUL}>
-            <li
-              onClick={() => {
-                window.open("mailto:bea.pitzschke.coden@gmail.com");
-              }}>
-              send Mail
-            </li>
-            <li
-              onClick={() => {
-                window.open("tel:+4915750000000");
-              }}>
-              call
-            </li>
-          </ul>
-        </li>
+        <li onClick={() => navigate("/newsletter", { state: { category: "newsletter" } })}>Newsletter</li>
       </ul>
     </div>
   );
