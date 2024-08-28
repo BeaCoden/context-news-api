e80123a469069d64efec44d7c31c66c7f91df521
Bea Pitzschke <bea.pitzschke.coden@gmail.com>
2024-08-28 05:43:58 +0200

    refactor: Handle error responses in Search component when fetching news articles

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -4,6 +4,7 @@ import { useLocation } from "react-router-dom";
 
 const Search = () => {
   const [news, setNews] = useState([]);
+  const [error, setError] = useState(null); // State für Fehlermeldungen
   const { state } = useLocation();
 
   const apiKey = process.env.REACT_APP_API_KEY;
@@ -11,8 +12,24 @@ const Search = () => {
 
   useEffect(() => {
     fetch(url)
-      .then((response) => response.json())
-      .then((data) => setNews(data.articles)); // Updated to directly set articles
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data.articles.length === 0) {
+          setError("Keine Nachrichten gefunden. Bitte überprüfen Sie Ihre Eingabe.");
+        } else {
+          setNews(data.articles);
+          setError(null); // Reset error if data is found
+        }
+      })
+      .catch((error) => {
+        setError("Ein Fehler ist aufgetreten. Bitte versuchen Sie es später erneut.");
+        console.error(error);
+      });
   }, [url]);
 
   console.log(news);
@@ -23,28 +40,35 @@ const Search = () => {
         <em>Showing news for:</em>
       </p>
       <h1>{state}</h1>
-      <div className={styles.newsGrid}>
-        {news.map((article, index) => (
-          <div
-            key={index}
-            className={styles.newsItem}>
-            <h2>
-              <em>{article.title}</em>
-            </h2>
-            <img
-              src={article.urlToImage}
-              alt={article.title}
-            />
-            <p>{article.description}</p>
-            <a
-              href={article.url}
-              target="_blank"
-              rel="noopener noreferrer">
-              Read more ➡️
-            </a>
-          </div>
-        ))}
-      </div>
+
+      {error ? (
+        <div className={styles.error}>
+          <p>{error}</p>
+        </div>
+      ) : (
+        <div className={styles.newsGrid}>
+          {news.map((article, index) => (
+            <div
+              key={index}
+              className={styles.newsItem}>
+              <h2>
+                <em>{article.title}</em>
+              </h2>
+              <img
+                src={article.urlToImage}
+                alt={article.title}
+              />
+              <p>{article.description}</p>
+              <a
+                href={article.url}
+                target="_blank"
+                rel="noopener noreferrer">
+                Read more ➡️
+              </a>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
