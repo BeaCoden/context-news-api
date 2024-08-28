2489db82d9367b3280aaaac6c515f5e9020acf67
Bea Pitzschke <bea.pitzschke.coden@gmail.com>
2024-08-28 04:51:11 +0200

    refactor: Add font size to body in index.css and fetch news articles in Search component

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,17 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Search.module.css";
 import { useLocation } from "react-router-dom";
 
 const Search = () => {
+  const [news, setNews] = useState([]);
   const { state } = useLocation();
-  console.log(state);
+
+  const apiKey = process.env.REACT_APP_API_KEY;
+  const url = `https://newsapi.org/v2/top-headlines?q=${state}&apiKey=${apiKey}`;
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setNews(data.articles)); // Updated to directly set articles
+  }, [url]);
+
+  console.log(news);
+
   return (
     <div className={styles.search}>
-      <h1>
-        News about:
-        <span>{}</span>
-      </h1>
-      <div></div>
+      <p>
+        <em>Showing news for:</em>
+      </p>
+      <h1>{state}</h1>
+      <div className={styles.newsGrid}>
+        {news.map((article, index) => (
+          <div
+            key={index}
+            className={styles.newsItem}>
+            <h2>
+              <em>{article.title}</em>
+            </h2>
+            <img
+              src={article.urlToImage}
+              alt={article.title}
+            />
+            <p>{article.description}</p>
+            <a
+              href={article.url}
+              target="_blank"
+              rel="noopener noreferrer">
+              Read more ➡️
+            </a>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
