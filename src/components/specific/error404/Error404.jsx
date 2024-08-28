c38386fa06341b39f7eeca72c3f02163a2ca9b22
Bea Pitzschke <bea.pitzschke.coden@gmail.com>
2024-08-28 14:23:09 +0200

    refactor: Update Error404 component with emojis and adjust styles

--- src/components/specific/error404/Error404.jsx
+++ src/components/specific/error404/Error404.jsx
@@ -7,13 +7,15 @@ const Error404 = () => {
     <div className={styles.container}>
       <div className={styles.errorContent}>
         <div className={styles.errorVisual}>
+          <div className={styles.fourLeft}>4</div>
           <div className={styles.moon}></div>
+          <div className={styles.fourRight}>4</div>
           <div className={`${styles.emoji} ${styles.rocket}`}>🚀</div>
           <div className={`${styles.emoji} ${styles.satellite}`}>🛰️</div>
           <div className={`${styles.emoji} ${styles.earth}`}>🌏</div>
           <div className={styles.stars}></div>
         </div>
-        <h4 className={styles.errorTitle}>404 - Lost in Space</h4>
+        <h4 className={styles.errorTitle}>Lost in Space</h4>
         <p className={styles.errorMessage}>Oh no! You seem to be stranded in space.</p>
         <p className={styles.errorMessage}>The page you are looking for does not exist. Please try another page.</p>
         <Link
