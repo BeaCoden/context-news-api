58de5540946eccde39c95ca5ceaf8a323638437b
Bea Pitzschke <bea.pitzschke.coden@gmail.com>
2024-09-06 23:54:11 +0200

    Refactor useFetchNews hook to use useRef for timer and improve fetching behavior

--- src/hooks/useFetchNews.jsx
+++ src/hooks/useFetchNews.jsx
@@ -1,43 +1,41 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 const useFetchNews = (url, delay = 300) => {
   const [news, setNews] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
-  const [timer, setTimer] = useState(null);
+  const timerRef = useRef(null);
 
   useEffect(() => {
-    if (timer) {
-      clearTimeout(timer);
-    }
-    const newTimer = setTimeout(() => {
-      const fetchNews = async () => {
-        setLoading(true);
-        try {
-          const response = await fetch(url);
-          if (!response.ok) {
-            throw new Error("Network response was not ok");
-          }
-          const data = await response.json();
-          if (data.articles.length === 0) {
-            setError("No news found. Please adjust your filters.");
-            setNews([]);
-          } else {
-            setNews(data.articles);
-            setError(null);
-          }
-        } catch (error) {
-          setError("An error has occurred. Please try again later.");
-          console.error(error);
-        } finally {
-          setLoading(false);
+    const fetchNews = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
         }
-      };
-      fetchNews();
-    }, delay);
-    setTimer(newTimer);
-    return () => clearTimeout(newTimer);
-  }, [url, delay, timer]);
+        const data = await response.json();
+        if (data.articles.length === 0) {
+          setError("No news found. Please adjust your filters.");
+          setNews([]);
+        } else {
+          setNews(data.articles);
+          setError(null);
+        }
+      } catch (error) {
+        setError("An error has occurred. Please try again later.");
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    clearTimeout(timerRef.current);
+
+    timerRef.current = setTimeout(fetchNews, delay);
+
+    return () => clearTimeout(timerRef.current);
+  }, [url, delay]);
 
   return { news, error, loading };
 };
