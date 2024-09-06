c7ade43b14a631e08a5c0951fffb3498d26d6e52
Bea Pitzschke <bea.pitzschke.coden@gmail.com>
2024-09-06 02:22:23 +0200

    Refactor useFetchNews hook to add loading state and delay fetching news from mock Data

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,32 +1,41 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+// import axios from "axios";
 import styles from "./Home.module.css";
 import Footer from "../../components/common/footer/Footer";
 import Slider from "../../components/common/slider/Slider";
 import Spinner from "../../components/common/spinner/Spinner";
 import NewsCard from "../../components/specific/newsCard/NewsCard";
+import { mockNews } from "../../mocks/mockNewsData";
 
 const Home = () => {
   const [news, setNews] = useState([]);
   const [loading, setLoading] = useState(false);
-  const apiKey = process.env.REACT_APP_API_KEY;
-  const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
+  // const apiKey = process.env.REACT_APP_API_KEY;
+  // const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
 
-  const fetchNews = async () => {
-    setLoading(true);
-    try {
-      const { data } = await axios.get(url);
-      setNews(data.articles);
-    } catch (error) {
-      console.error("Failed to fetch news:", error);
-    } finally {
-      setLoading(false);
-    }
-  };
+  // const fetchNews = async () => {
+  //   setLoading(true);
+  //   try {
+  //     const { data } = await axios.get(url);
+  //     setNews(data.articles);
+  //   } catch (error) {
+  //     console.error("Failed to fetch news:", error);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   fetchNews();
+  //   //eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
 
   useEffect(() => {
-    fetchNews();
-    //eslint-disable-next-line react-hooks/exhaustive-deps
+    setLoading(true);
+    setTimeout(() => {
+      setNews(mockNews); // Verwenden von Mock-Daten
+      setLoading(false);
+    }, 1000); // Simulieren einer Ladezeit
   }, []);
 
   return (
@@ -34,11 +43,12 @@ const Home = () => {
       <div className={styles.slider}>
         <Slider />
       </div>
-      <div>
-        <h1>News</h1>
-        <div className={styles.newsCard}>
-          {loading && <Spinner />}
-          {news?.map((article, index) => (
+      <h1>News</h1>
+      <div className={styles.newsCard}>
+        {loading ? (
+          <Spinner />
+        ) : (
+          news.map((article, index) => (
             <NewsCard
               key={index}
               title={article.title}
@@ -46,8 +56,8 @@ const Home = () => {
               imageUrl={article.urlToImage}
               url={article.url}
             />
-          ))}
-        </div>
+          ))
+        )}
       </div>
       <Footer />
     </div>
