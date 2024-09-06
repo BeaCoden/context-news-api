8dadbe9e85057f0f9b757b006ac7d26d9f8bd65e
Bea Pitzschke <bea.pitzschke.coden@gmail.com>
2024-09-06 23:53:21 +0200

    Refactor Home component to improve loading experience and handle query changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,63 +1,94 @@
-import React, { useEffect, useState } from "react";
-// import axios from "axios";
+import React, { useState, useEffect, useRef } from "react";
+import { useLocation } from "react-router-dom";
 import styles from "./Home.module.css";
 import Footer from "../../components/common/footer/Footer";
 import Slider from "../../components/common/slider/Slider";
 import Spinner from "../../components/common/spinner/Spinner";
 import NewsCard from "../../components/specific/newsCard/NewsCard";
-import { mockNews } from "../../mocks/mockNewsData";
+import useFetchNews from "../../hooks/useFetchNews";
 
 const Home = () => {
-  const [news, setNews] = useState([]);
+  const { state } = useLocation();
   const [loading, setLoading] = useState(false);
-  // const apiKey = process.env.REACT_APP_API_KEY;
-  // const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
+  const [showContent, setShowContent] = useState(false);
+  const prevQuery = useRef(state);
 
-  // const fetchNews = async () => {
-  //   setLoading(true);
-  //   try {
-  //     const { data } = await axios.get(url);
-  //     setNews(data.articles);
-  //   } catch (error) {
-  //     console.error("Failed to fetch news:", error);
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // };
+  const apiKey = process.env.REACT_APP_API_KEY;
+  const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
 
-  // useEffect(() => {
-  //   fetchNews();
-  //   //eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []);
+  const { news, error } = useFetchNews(url);
 
   useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      setNews(mockNews); // Verwenden von Mock-Daten
+    const timer = setTimeout(() => {
       setLoading(false);
-    }, 1000); // Simulieren einer Ladezeit
+    }, 3000);
+    return () => clearTimeout(timer);
   }, []);
+
+  useEffect(() => {
+    if (!loading && (news.length || error)) {
+      setShowContent(true);
+    }
+  }, [loading, news, error]);
+
+  useEffect(() => {
+    if (state !== prevQuery.current) {
+      setLoading(true);
+      setShowContent(false);
+      prevQuery.current = state;
+    }
+  }, [state]);
 
   return (
     <div className={styles.container}>
       <div className={styles.slider}>
         <Slider />
       </div>
-      <h1>News</h1>
-      <div className={styles.newsCard}>
-        {loading ? (
-          <Spinner />
-        ) : (
-          news.map((article, index) => (
-            <NewsCard
-              key={index}
-              title={article.title}
-              description={article.description}
-              imageUrl={article.urlToImage}
-              url={article.url}
-            />
-          ))
-        )}
+      <div>
+        <h1>News</h1>
+        <div className={styles.newsCard}>
+          {loading ? (
+            <div className={styles.loading}>
+              <Spinner />
+            </div>
+          ) : showContent && error ? (
+            <div className={styles.error}>
+              <p>
+                We couldn't find any articles <br /> matching your search.
+              </p>
+              <div className={styles.buttonContainer}>
+                <a
+                  className={styles.button}
+                  href="/categories">
+                  search in categories
+                </a>
+                <button
+                  className={styles.button}
+                  onClick={() => (window.location.href = "/")}>
+                  back Home
+                </button>
+              </div>
+            </div>
+          ) : showContent && !news.length ? (
+            <div className={styles.loading}>
+              <p>No news articles found...</p>
+            </div>
+          ) : (
+            showContent && (
+              <div className={styles.newsGrid}>
+                {news.map((article, index) => (
+                  <NewsCard
+                    key={index}
+                    title={article.title}
+                    description={article.description}
+                    imageUrl={article.urlToImage}
+                    url={article.url}
+                  />
+                ))}
+              </div>
+            )
+          )}
+        </div>
       </div>
       <Footer />
     </div>
