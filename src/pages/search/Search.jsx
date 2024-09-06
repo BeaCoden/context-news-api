51d9ab8c7a05a3a268dde36a3e16b72e941899f9
Bea Pitzschke <bea.pitzschke.coden@gmail.com>
2024-09-06 23:53:09 +0200

    Refactor Search component to improve loading experience and handle query changes

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -2,42 +2,40 @@ import React, { useState, useEffect, useRef } from "react";
 import styles from "./Search.module.css";
 import { useLocation } from "react-router-dom";
 import NewsCard from "../../components/specific/newsCard/NewsCard";
-// import useFetchNews from "../../hooks/useFetchNews";
-// import Spinner from "../../components/common/spinner/Spinner";
-import { mockNews } from "../../mocks/mockNewsData";
+import useFetchNews from "../../hooks/useFetchNews";
+import Spinner from "../../components/common/spinner/Spinner";
 
 const Search = () => {
   const { state } = useLocation();
-  // const [loading, setLoading] = useState(true);
-  // const [showContent, setShowContent] = useState(false);
-  // const prevQuery = useRef(state);
+  const [loading, setLoading] = useState(true);
+  const [showContent, setShowContent] = useState(false);
+  const prevQuery = useRef(state);
 
-  // const apiKey = process.env.REACT_APP_API_KEY;
-  // const url = `https://newsapi.org/v2/top-headlines?q=${state}&apiKey=${apiKey}`;
+  const apiKey = process.env.REACT_APP_API_KEY;
+  const url = `https://newsapi.org/v2/top-headlines?q=${state}&apiKey=${apiKey}`;
 
-  // const { news, error } = useFetchNews(url);
-  const [news, setNews] = useState(mockNews);
+  const { news, error } = useFetchNews(url);
 
-  // useEffect(() => {
-  //   const timer = setTimeout(() => {
-  //     setLoading(false);
-  //   }, 3000);
-  //   return () => clearTimeout(timer);
-  // }, []);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
 
-  // useEffect(() => {
-  //   if (!loading && (news.length || error)) {
-  //     setShowContent(true);
-  //   }
-  // }, [loading, news, error]);
+  useEffect(() => {
+    if (!loading && (news.length || error)) {
+      setShowContent(true);
+    }
+  }, [loading, news, error]);
 
-  // useEffect(() => {
-  //   if (state !== prevQuery.current) {
-  //     setLoading(true);
-  //     setShowContent(false);
-  //     prevQuery.current = state;
-  //   }
-  // }, [state]);
+  useEffect(() => {
+    if (state !== prevQuery.current) {
+      setLoading(true);
+      setShowContent(false);
+      prevQuery.current = state;
+    }
+  }, [state]);
 
   return (
     <div className={styles.search}>
@@ -45,16 +43,49 @@ const Search = () => {
         <em>Showing news for:</em>
       </p>
       <h1>{state || "All News"}</h1>
-      <div className={styles.newsGrid}>
-        {news.map((article, index) => (
-          <NewsCard
-            key={index}
-            title={article.title}
-            description={article.description}
-            imageUrl={article.urlToImage}
-            url={article.url}
-          />
-        ))}
+
+      <div className={styles.newsCard}>
+        {loading ? (
+          <div className={styles.loading}>
+            <Spinner />
+          </div>
+        ) : showContent && error ? (
+          <div className={styles.error}>
+            <p>
+              We couldn't find any articles <br /> matching your search.
+            </p>
+            <div className={styles.buttonContainer}>
+              <a
+                className={styles.button}
+                href="/categories">
+                search in categories
+              </a>
+              <button
+                className={styles.button}
+                onClick={() => (window.location.href = "/")}>
+                back Home
+              </button>
+            </div>
+          </div>
+        ) : showContent && !news.length ? (
+          <div className={styles.loading}>
+            <p>No news articles found...</p>
+          </div>
+        ) : (
+          showContent && (
+            <div className={styles.newsGrid}>
+              {news.map((article, index) => (
+                <NewsCard
+                  key={index}
+                  title={article.title}
+                  description={article.description}
+                  imageUrl={article.urlToImage}
+                  url={article.url}
+                />
+              ))}
+            </div>
+          )
+        )}
       </div>
     </div>
   );
